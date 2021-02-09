b40271c04d9960a7bd4d00bb365c48abb56f045e
Guillaume <guillaume.toussaint1@etu.univ-lorraine.fr>
2021-02-09 19:00:09 +0100

    Changement async connexion

--- controller/ConnectionController.js
+++ controller/ConnectionController.js
@@ -3,7 +3,7 @@ module.exports =  {
 
 
 
-  sign_in(username, pass) {
+    async sign_in(username, pass,req) {
     let SHA512 = require("crypto-js/sha512");
 
     //Conenciton à la bdd
@@ -14,8 +14,25 @@ module.exports =  {
 
     db = require("./DatabaseConnection.js").createConnection();
 
+    let requete = "Select * from Utilisateur where pseudo='"+username+"' and mdp='"+passw+"';";
+    console.log("Requête de connexion : "+requete);
 
+    await db.query(requete, function (err, result) {
+    if (err) throw err;
 
+    if(result[0]){
+      console.log("Connexion de  : "+result[0].pseudo);
+      return result[0].pseudo;
+    }
+    });
+    //console.log("Connexion réussie");
+    //console.log("Connexion de  : "+reussite);
+    /*if(reussite[0]){
+      return reussite[0].pseudo;
+    }else{
+      return null;
+    }
+    */
 
   },
 
